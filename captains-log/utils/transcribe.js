31ead5abd06410eb0c80588f6feb3287d7f39bfa
Gera Yeremin <gera3d@gmail.com>
2025-03-27 20:58:23 -0700

    Add captains-log/utils/transcribe.js

---
+++ captains-log/utils/transcribe.js
@@ -0,0 +1,5 @@
+export async function transcribeAudio(audioURL) {
+  // Placeholder function for transcribing audio
+  // In a real application, this would call an external API
+  return 'This is a transcribed text from the audio.';
+}
